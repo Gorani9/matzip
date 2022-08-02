3a0a01f2a25e7623cf5153c5d7d3938cf445fb26
Goniieee <gksrhs97@gmail.com>
2022-08-02 21:09:03 +0900

    fix: scripts, ci, cd

--- scripts/health.sh
+++ scripts/health.sh
@@ -15,7 +15,7 @@ sleep 10
 for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:"${IDLE_PORT}"/ping/)
-  UP_COUNT=$(echo "${RESPONSE}" | grep -c 'prod')
+  UP_COUNT=$(echo "${RESPONSE}" | grep -c 'pong')
 
   if [ "${UP_COUNT}" -ge 1 ]
   then
