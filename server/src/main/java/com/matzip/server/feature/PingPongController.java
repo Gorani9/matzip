3f48b47f884215580843352900b2021724c4566f
Goniieee <gksrhs97@gmail.com>
2022-08-03 23:51:38 +0900

    feat: add https settings

--- server/src/main/java/com/matzip/server/feature/PingPongController.java
+++ server/src/main/java/com/matzip/server/feature/PingPongController.java
@@ -4,10 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/api/ping")
 public class PingPongController {
-    @GetMapping("/ping/")
+    @GetMapping("/")
     public ResponseEntity<String> pingTest() {
         return new ResponseEntity<>("pong", HttpStatus.OK);
     }
